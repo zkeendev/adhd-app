a9479b1d4442742c8b837aa1bec9c5a6c4b379fe
Zach Keen <zachmichaelkeen@gmail.com>
2025-07-14 19:36:40 -0700

    added timezone field

--- client/lib/features/user_profile/user_profile.dart
+++ client/lib/features/user_profile/user_profile.dart
@@ -7,6 +7,7 @@ class UserProfile {
   final String uid;
   final String? email;
   final String? displayName;
+  final String? timezone;
   final Timestamp? createdAt;
   final List<String>? fcmTokens;
 
@@ -14,6 +15,7 @@ class UserProfile {
     required this.uid,
     this.email,
     this.displayName,
+    this.timezone,
     this.createdAt,
     this.fcmTokens,
   });
@@ -23,6 +25,7 @@ class UserProfile {
       'uid': uid,
       'email': email,
       'displayName': displayName,
+      'timezone': timezone,
       'createdAt': createdAt ?? FieldValue.serverTimestamp(),
       fcmTokensField: fcmTokens ?? [],
     };
@@ -33,10 +36,12 @@ class UserProfile {
       uid: json['uid'] as String,
       email: json['email'] as String?,
       displayName: json['displayName'] as String?,
+      timezone: json['timezone'] as String?,
       createdAt: json['createdAt'] as Timestamp?,
-      fcmTokens: json[fcmTokensField] != null
-          ? List<String>.from(json[fcmTokensField] as List)
-          : null,
+      fcmTokens:
+          json[fcmTokensField] != null
+              ? List<String>.from(json[fcmTokensField] as List)
+              : null,
     );
   }
 
@@ -47,6 +52,7 @@ class UserProfile {
         other.uid == uid &&
         other.email == email &&
         other.displayName == displayName &&
+        other.timezone == timezone &&
         other.createdAt == createdAt &&
         other.fcmTokens == fcmTokens;
   }
@@ -56,6 +62,7 @@ class UserProfile {
     return uid.hashCode ^
         email.hashCode ^
         displayName.hashCode ^
+        timezone.hashCode ^
         createdAt.hashCode ^
         fcmTokens.hashCode;
   }
